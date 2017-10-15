b790d607da3270ed01d0fafdcfff2fe0abda8b5b
eeefan <e.fan.arex@gmail.com>
2017-10-16 01:47:13 +0800

    Initial Commit

---
+++ allsafe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  allsafe
+//
+//  Created by eeefan on 2017/10/16.
+//  Copyright © 2017年 eeefan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
